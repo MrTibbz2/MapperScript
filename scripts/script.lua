d7fd08df9a6ee82e820a60385b5c591713b46cf3
MrTibbz2 <Mrtibblles@gmail.com>
2025-08-03 09:54:12 +1000

    copyright

--- scripts/script.lua
+++ scripts/script.lua
@@ -1,1 +1,4 @@
+-- Copyright (c) 2025 Lachlan McKenna
+-- All rights reserved. No part of this code may be used, copied, or distributed without permission.
+
 print("it fucking worked gang.")
--- src/Scripting/ScriptManager.cpp
+++ src/Scripting/ScriptManager.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 Lachlan McKenna
+// All rights reserved. No part of this code may be used, copied, or distributed without permission.
+
+
 #include "ScriptManager.h"
 #include <iostream>
 #include <format>
